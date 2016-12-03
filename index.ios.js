c4a989eef5f684cd9d75b941de1e8f0da800f0e6
rav <korzewski@gmail.com>
2016-12-03 12:13:08 +0100

    add tabs navigation

--- index.ios.js
+++ index.ios.js
@@ -8,24 +8,27 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  View,
 } from 'react-native';
+
+import {
+  createRouter,
+  NavigationProvider,
+} from '@exponent/ex-navigation';
+
+import Navigation from './src/Navigation';
+
+const Router = createRouter(() => ({
+  home: () => HomeScreen,
+}));
 
 export default class teamRadek extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <NavigationProvider router={Router}>
+          <Navigation />
+        </NavigationProvider>
       </View>
     );
   }
@@ -34,20 +37,7 @@ export default class teamRadek extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('teamRadek', () => teamRadek);
